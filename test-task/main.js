4c700b754f59fdba1d33dc15694e362453ff5a48
sacd <artemie007@mail.ru>
2018-03-16 22:09:05 +0300

    Update main.js

--- test-task/main.js
+++ test-task/main.js
@@ -1,6 +1,7 @@
 
+// Заполнение прогресс баров
 var elements = document.querySelectorAll('.progress-bar');
-Array.prototype.forEach.call(elements, function(el, i){
+Array.prototype.forEach.call(elements, function(el,i){
   var progress = elements[i].getAttribute('data-progress');
   elements[i].children[0].style.width = progress+'%';
   var form = elements[i].parentNode ;
@@ -9,38 +10,35 @@ Array.prototype.forEach.call(elements, function(el, i){
 
 });
 
-document.querySelector('.dec').addEventListener('click', function() { 
-	var form = this.parentNode ;
-	var count = parseInt(form.querySelector('.num').innerHTML);
-	if (count > 1) {
-		count--;
-		form.querySelector('.num').innerHTML = count;
-	} else {
-		// if (form.querySelector('.dec').classList)
-			  form.querySelector('.dec').classList.add('disable');
-		// 	else
-		// 	  form.querySelector('.dec').disable += ' ' + disable;
-		
-		}
-}, false);
+// Уменьшить значение
+document.querySelectorAll('.dec').forEach(function(elem) { elem.addEventListener('click', function(event) { 
+		var form = this.parentNode ;
+		var count = parseInt(form.querySelector('.num').innerHTML);
+		if (count > 1) {
+			count--;
+			form.querySelector('.num').innerHTML = count;
+		} else {
+				  form.querySelector('.dec').classList.add('disable');
+			}
+		}, false)
+});
 
+// Увеличить значение
+document.querySelectorAll('.inc').forEach(function(elem) { elem.addEventListener('click', function(event) { 
 
-document.querySelector('.inc').addEventListener('click', function() { 
 	var form = this.parentNode ;
 	var count = parseInt(form.querySelector('.num').innerHTML);
 		count++;
 		form.querySelector('.num').innerHTML = count++;
 		if (count > 1){
-		 // form.querySelector('.num').classList.remove('disable');
-			// if (form.querySelector('.dec').classList)
 			  form.querySelector('.dec').classList.remove('disable');
-			// else
-			//   form.querySelector('.dec').disable = el.disable.replace(new RegExp('(^|\\b)' + disable.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 			}
 		
 }, false);
+});
 
-document.querySelector('.btn').addEventListener('click', function() { 
+// Конпка удалить
+document.querySelectorAll('.btn').forEach(function(elem) { elem.addEventListener('click', function(event) { 
 		var form = this.parentNode.parentNode ;
 
 		if (form.classList.contains('remove')) {
@@ -65,3 +63,4 @@ document.querySelector('.btn').addEventListener('click', function() {
 			//   form.remove = classes.join(' ');
 			// }
 	}, false);
+});
